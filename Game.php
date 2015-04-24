97800d94b7460315f42445222e5a79a5783bc8e3
France Wang <france.fs.wang@gmail.com>
2015-04-24 18:20:35 +0200

    added project files

---
+++ Game.php
@@ -0,0 +1,72 @@
+<?php
+class Game
+{
+    private $state;
+    private $nextState;
+    private $c;
+    private $r; 
+
+    public function __construct($initialState = [[]])
+    {
+        $this->state = $initialState;
+        $this->r = count($initialState);
+        $this->c = count($initialState[0]);
+        $this->nextState = $initialState;
+    }
+
+    public function getState()
+    {
+    	return $this->state;
+    }
+
+    public function getNextState()
+    {
+        for($y=0; $y < $this->r; $y++){
+            for($x=0; $x < $this->c; $x++){
+                $this->nextState[$y][$x] = $this->updateCellState($x, $y);
+            }
+        }
+        return $this->nextState;
+    }
+
+    public function updateCellState($x, $y)
+    {
+        if($this->state[$y][$x] == 1){
+            if(($this->countCellNeighbours($x, $y) < 2)||($this->countCellNeighbours($x, $y) > 3)){
+                return 0;
+            }
+        }
+        else{
+            if($this->countCellNeighbours($x, $y) == 3){
+                return 1;
+            }
+        }
+        return $this->state[$y][$x];
+    }
+
+    public function countCellNeighbours($x, $y)
+    {
+        $neighbourCount = 0;
+
+        for($j=-1; $j<=1; $j++){
+            for($i=-1; $i<=1; $i++){
+
+                try{
+                    if($this->state[$y+$j][$x+$i] == 1){
+                        {
+                            if(!(($i==0) && ($j==0))){
+                                $neighbourCount++;
+                            }
+                        }
+                    }
+                }
+                catch(Exception $e){
+
+                }
+            }
+        }
+
+        return $neighbourCount;
+    }
+
+}
---
+++ GameTest.php
@@ -0,0 +1,144 @@
+<?php
+class GameTest extends PHPUnit_Framework_TestCase
+{
+    public function testGameInitialization_WithEmptyState()
+    {
+        $game = new Game();
+
+        $state = $game->getState();
+
+        $this->assertEquals($state, [[]]);
+    }
+
+    public function testGameIsInitialization_WithInitialState()
+    {
+    	$initialState = array(
+            array(0,0,0,0),
+            array(0,0,0,0),
+            array(0,0,0,0),
+            array(0,0,0,0)
+            );
+
+    	$game = new Game($initialState);
+
+    	$state = $game->getState();
+
+    	$this->assertEquals($state, $initialState);
+    }
+
+    public function testNeighbourCounting()
+    {
+        $initialState = array(
+            array(1,1),
+            array(0,1));
+
+        $game = new Game($initialState);
+        $neighbourCount = $game->countCellNeighbours(1,0);
+
+        $this->assertEquals($neighbourCount, 2);
+    }
+
+    public function testNextState_NoEvolution()
+    {
+        $initialState = array(
+            array(0,0,0,0,0,0,0,0),
+            array(0,0,0,0,0,0,0,0),
+            array(0,0,0,0,0,0,0,0),
+            array(0,0,0,0,0,0,0,0));
+
+        $game = new Game($initialState);
+
+        $nextState = $game->getNextState();
+
+        $this->assertEquals($initialState, $nextState);
+    }
+
+    public function testCellUpdtate_CellDies_Underpopulation()
+    {
+        $initialState = array(
+            array(0,0,0),
+            array(0,1,0),
+            array(0,0,0));
+
+        $game = new Game($initialState);
+
+        $newCellState = $game->updateCellState(1,1);
+
+        $this->assertEquals($newCellState, 0);
+    }
+
+    public function testCellUpdtate_CellDies_Overcrowding()
+    {
+        $initialState = array(
+            array(1,1,1),
+            array(1,1,0),
+            array(0,0,0));
+
+        $game = new Game($initialState);
+
+        $newCellState = $game->updateCellState(1,1);
+
+        $this->assertEquals($newCellState, 0);
+    }
+
+    public function testCellUpdtate_CellSurvives_TwoNeighbours()
+    {
+        $initialState = array(
+            array(1,1),
+            array(0,1));
+
+        $game = new Game($initialState);
+
+        $newCellState = $game->updateCellState(1,1);
+
+        $this->assertEquals($newCellState, 1);
+    }
+
+    public function testCellUpdtate_CellSurvives_ThreeNeighbours()
+    {
+        $initialState = array(
+            array(1,1),
+            array(1,1));
+
+        $game = new Game($initialState);
+
+        $newCellState = $game->updateCellState(1,1);
+
+        $this->assertEquals($newCellState, 1);
+    }
+
+    public function testCellUpdtate_CellPopulates()
+    {
+        $initialState = array(
+            array(1,1),
+            array(0,1));
+
+        $game = new Game($initialState);
+
+        $newCellState = $game->updateCellState(1,0);
+
+        $this->assertEquals($newCellState, 1);
+    }
+
+    public function testNextState()
+    {
+        $initialState = array(
+            array(1,1,0,0),
+            array(0,1,1,0),
+            array(1,1,0,1),
+            array(0,1,1,0));
+
+        $expectedState = array(
+            array(1,1,1,0),
+            array(0,0,0,0),
+            array(1,0,0,1),
+            array(1,1,1,0));
+
+        $game = new Game($initialState);
+
+        $newState = $game->getNextState();
+
+        $this->assertEquals($newState, $expectedState);
+    }
+
+}
